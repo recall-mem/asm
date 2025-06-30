694b5c5d942958b29995820b55dd98d2c063938a
recall-mem <abbas_3211@mail.ru>
2025-07-01 00:49:15 +0400

    Delete x86-nasm/Dersler directory

--- x86-nasm/Dersler/11_floating_point.asm
+++
@@ -1,8 +0,0 @@
-section .data
-    x DD 3.14
-    y DD 2.1
-
-section .text
-global _start
-
-_start:
--- x86-nasm/Dersler/9_signed_unsigned.asm
+++
@@ -1,7 +0,0 @@
-section .text
-global _start
-
-_start:
-    MOV cl, 0b01111111
-    MOV eax, 1
-    INT 0x80
